4bcbe6ec75665136b7019bc8f01e43ae7b63dad3
Matheus Soares Silveira <soaressilveira.matheus@gmail.com>
2020-08-30 22:23:21 -0300

    teste

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Teste Development';
+    return 'Hello CI CD!';
   }
 }
